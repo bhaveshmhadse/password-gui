a38c5a6b087075bf2bd887869230f71a6fd39543
bhaveshmhadse <bhaveshmhadse9@gmail.com>
2022-03-03 22:46:41 +0530

    Made Password gui

--- components/Matrix/Matrix.tsx
+++ components/Matrix/Matrix.tsx
@@ -5,9 +5,9 @@ const Matrix = ({ password, setPassword, matrix, setMatrix }: { matrix: any[][];
     <div className='lg:w-1/2'>
       {matrix.map((eachArray, row) => {
         return (
-          <div className='grid grid-cols-3 w-full lg:gap-3'>
+          <div key={Math.random().toString()} className='grid grid-cols-3 w-full lg:gap-3'>
             {eachArray.map((eachBlock, col) => (
-              <Block title={eachBlock.title} position={[row, col]} matrix={matrix} setMatrix={setMatrix} isSelected={eachBlock.isSelected} password={password} setPassword={setPassword} />
+              <Block key={Math.random().toString()} title={eachBlock.title} position={[row, col]} matrix={matrix} setMatrix={setMatrix} isSelected={eachBlock.isSelected} password={password} setPassword={setPassword} />
             ))}
           </div>
         );
